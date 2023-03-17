625a5effff9fc20effcc1ae4f7f6ac32df2b2161
Andreas Müller <schnitzeltony@gmail.com>
2023-03-17 22:28:02 +0100

    advancednetworkmanger: Give up auto registration
    It never worked for standard builds setting as-needed compile flags and
    discussions with ingenious hard heads are not to expect.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/advancednetworkmanger_lib/src/advancednetworkmanager.h
+++ modules/advancednetworkmanger_lib/src/advancednetworkmanager.h
@@ -1,37 +1,16 @@
 #ifndef ADVANCEDNETWORKMANAGER_H
 #define ADVANCEDNETWORKMANAGER_H
 
-#include "connectiontreeinterface.h"
-#include "connectionmodel.h"
-#include "abstractconnectionsettingsinterface.h"
-#include "wiredconnectionsettingsinterface.h"
-#include "wirelessconnectionsettingsinterface.h"
-#include "infointerface.h"
-#include "networkmanagerabstraction.h"
-#include "nmcppnotification.h"
+#include <QObject>
 
-#include <QtQml/QQmlExtensionPlugin>
-
-class AdvancedNetworkmanager : public QQmlExtensionPlugin {
+class AdvancedNetworkmanager : public QObject
+{
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "my.plugin.example/1.0")
-
 public:
-    /**
-     * @brief init library
-     *
-     * This function will init the library and
-     * register the c++ classes.
-     * It is automaticly called by QT if the library is linked.
-     *
-     */
-    static void init();
-    /**
-     * @brief registerTypes Overrided function that should register all
-     * C++ classes exported by this plugin.
-     * @param uri           Plugin uri.
-     */
-    void registerTypes(const char* uri) override;
+    static void registerQml();
+private:
+    static void registerTypes(const char* uri);
+    static bool m_wasRegistered;
 };
 
 #endif // ADVANCEDNETWORKMANAGER_H
