b9faf420d2b3c0520f3ed09a397eb0c51db38f46
Andreas Müller <schnitzeltony@gmail.com>
2023-03-17 22:28:02 +0100

    advancednetworkmanger: Give up auto registration
    It never worked for standard builds setting as-needed compile flags and
    discussions with ingenious hard heads are not to expect.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/advancednetworkmanger_lib/src/advancednetworkmanager.cpp
+++ modules/advancednetworkmanger_lib/src/advancednetworkmanager.cpp
@@ -1,32 +1,36 @@
 #include "advancednetworkmanager.h"
-#include <NetworkManagerQt/Device>
-#include <QCoreApplication>
+#include "abstractconnectionsettingsinterface.h"
+#include "connectiontreeinterface.h"
+#include "wiredconnectionsettingsinterface.h"
+#include "wirelessconnectionsettingsinterface.h"
+#include "infointerface.h"
+#include "networkmanagerabstraction.h"
+#include "nmcppnotification.h"
 #include <QtQml/QtQml>
 #include <zeratranslationplugin.h>
 
-void AdvancedNetworkmanager::init()
-{
-    AdvancedNetworkmanager* obj= new AdvancedNetworkmanager();
-    obj->registerTypes("anmsettings");
-}
+bool AdvancedNetworkmanager::m_wasRegistered = false;
 
 void AdvancedNetworkmanager::registerTypes(const char* uri)
 {
-    // Dependencies
-    ZeraTranslationPlugin::registerQml();
-    // Own
-    qmlRegisterType<AbstractConnectionSettingsInterface>(uri, 1, 0, "AbstractConnectionSettingsInterface");
-    qmlRegisterType<ConnectionModel>(uri, 1, 0, "ConnectionModel");
-    qmlRegisterType<ConnectionTreeInterface>(uri, 1, 0, "ConnectionTreeInterface");
-    qmlRegisterType<WiredConnectionSettingsInterface>(uri, 1, 0, "WiredConnectionSettingsInterface");
-    qmlRegisterType<WirelessConnectionSettingsInterface>(uri, 1, 0, "WirelessConnectionSettingsInterface");
-    qmlRegisterType<InfoInterface>(uri, 1, 0, "InfoInterface");
-    qmlRegisterType<NetworkmanagerAbstraction>(uri, 1, 0, "NetworkmanagerAbstraction");
-    qmlRegisterType<NmCppNotification>(uri,1, 0, "NmCppNotification");
+    if(!m_wasRegistered) {
+        // Dependencies
+        ZeraTranslationPlugin::registerQml();
+        // Own
+        qmlRegisterType<AbstractConnectionSettingsInterface>(uri, 1, 0, "AbstractConnectionSettingsInterface");
+        qmlRegisterType<ConnectionModel>(uri, 1, 0, "ConnectionModel");
+        qmlRegisterType<ConnectionTreeInterface>(uri, 1, 0, "ConnectionTreeInterface");
+        qmlRegisterType<WiredConnectionSettingsInterface>(uri, 1, 0, "WiredConnectionSettingsInterface");
+        qmlRegisterType<WirelessConnectionSettingsInterface>(uri, 1, 0, "WirelessConnectionSettingsInterface");
+        qmlRegisterType<InfoInterface>(uri, 1, 0, "InfoInterface");
+        qmlRegisterType<NetworkmanagerAbstraction>(uri, 1, 0, "NetworkmanagerAbstraction");
+        qmlRegisterType<NmCppNotification>(uri,1, 0, "NmCppNotification");
+
+        m_wasRegistered = true;
+    }
 }
 
-
-static void init(){
-    AdvancedNetworkmanager::init();
+void AdvancedNetworkmanager::registerQml()
+{
+    registerTypes("anmsettings");
 }
-Q_COREAPP_STARTUP_FUNCTION(init)
