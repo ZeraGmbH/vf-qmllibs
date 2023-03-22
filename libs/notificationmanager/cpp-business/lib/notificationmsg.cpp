15dc11ce7b6e087ef11363b6290b5d254f559ed5
Asmae Kaouissi <a.kaouissi@zera.de>
2023-03-22 16:53:42 +0100

    notificationmsg: Make Qt variables accessible to QML using Q_PROPERTY
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- libs/notificationmanager/cpp-business/lib/notificationmsg.cpp
+++ libs/notificationmanager/cpp-business/lib/notificationmsg.cpp
@@ -17,3 +17,81 @@ int NotificationMsg::getId() const
 {
     return m_id;
 }
+
+QColor NotificationMsg::getBackGroundColor()
+{
+    return m_backGroundColor;
+}
+
+QColor NotificationMsg::getBorderColor()
+{
+    return m_borderColor;
+}
+
+QColor NotificationMsg::getTextColor()
+{
+    return m_textColor ;
+}
+
+int NotificationMsg::getRadius()
+{
+    return m_radius;
+}
+
+int NotificationMsg::verticalAlignment()
+{
+    return m_verticalAlignment;
+}
+
+int NotificationMsg::horizontalAlignment()
+{
+    return m_horizontalAlignment;
+}
+
+void NotificationMsg::setBackGroundColor(QColor backGroundColor)
+{
+    if(m_backGroundColor != backGroundColor){
+        m_backGroundColor = backGroundColor;
+        emit backGroundColorChanged();
+    }
+}
+
+void NotificationMsg::setBorderColor(QColor borderColor)
+{
+    if(m_borderColor != borderColor){
+        m_borderColor = borderColor;
+        emit borderColorChanged();
+    }
+}
+
+void NotificationMsg::setTextColor(QColor textColor)
+{
+    if(m_textColor != textColor){
+        m_textColor = textColor;
+        emit textColorChanged();
+    }
+}
+
+void NotificationMsg::setRadius(int radius)
+{
+    if(m_radius != radius){
+        m_radius = radius;
+        emit radiusChanged();
+    }
+}
+
+void NotificationMsg::setVerticalAlignment(int verticalAlignment)
+{
+    if(m_verticalAlignment != verticalAlignment){
+        m_verticalAlignment = verticalAlignment;
+        emit verticalAlignmentChanged();
+    }
+}
+
+void NotificationMsg::setHorizontalAlignment(int horizontalAlignment)
+{
+    if(m_horizontalAlignment != horizontalAlignment){
+        m_horizontalAlignment = horizontalAlignment;
+        emit horizontalAlignmentChanged();
+    }
+}
--- libs/notificationmanager/cpp-business/lib/notificationmsg.h
+++ libs/notificationmanager/cpp-business/lib/notificationmsg.h
@@ -2,6 +2,7 @@
 #define NOTIFICATIONMSG_H
 
 #include <QObject>
+#include <QColor>
 #include <memory>
 
 class NotificationMsg : public QObject
@@ -11,12 +12,53 @@ public:
     NotificationMsg(QString msg);
     QString getMsg() const;
     int getId() const;
+
+    Q_PROPERTY(QColor backGroundColor READ getBackGroundColor WRITE setBackGroundColor NOTIFY backGroundColorChanged)
+    Q_PROPERTY(QColor borderColor READ getBorderColor WRITE setBorderColor NOTIFY borderColorChanged)
+    Q_PROPERTY(QColor textColor READ getTextColor WRITE setTextColor NOTIFY textColorChanged)
+    Q_PROPERTY(QString text READ getMsg)
+    Q_PROPERTY(int radius READ getRadius WRITE setRadius NOTIFY radiusChanged)
+    //Q_PROPERTY(item anchorsCenter READ anchorsCenter WRITE setAnchorsCenter NOTIFY anchorsCenterChanged)
+    Q_PROPERTY(int verticalAlignment READ verticalAlignment WRITE setVerticalAlignment NOTIFY verticalAlignmentChanged)
+    Q_PROPERTY(int horizontalAlignment READ horizontalAlignment WRITE setHorizontalAlignment NOTIFY horizontalAlignmentChanged)
+
+    QColor getBackGroundColor();
+    QColor getBorderColor();
+    QColor getTextColor();
+    int getRadius();
+    //item anchorsCenter();
+    int verticalAlignment();
+    int horizontalAlignment();
+
+    void setBackGroundColor(QColor backGroundColor);
+    void setBorderColor(QColor borderColor);
+    void setTextColor(QColor textColor);
+    void setRadius(int radius);
+    void setVerticalAlignment(int verticalAlignment);
+    void setHorizontalAlignment(int horizontalAlignment);
 signals:
     void sigFadeOut(int id);
+
+    void backGroundColorChanged();
+    void borderColorChanged();
+    void textColorChanged();
+    void radiusChanged();
+    void anchorsCenterChanged();
+    void verticalAlignmentChanged();
+    void horizontalAlignmentChanged();
+
 protected:
     const int m_id;
 private:
     const QString m_msg;
+
+    QColor m_backGroundColor;
+    QColor m_borderColor;
+    QColor m_textColor;
+    int m_radius;
+    //item m_anchorsCenter;
+    int m_verticalAlignment;
+    int m_horizontalAlignment;
 };
 
 typedef std::shared_ptr<NotificationMsg> NotificationMsgPtr;
