52021307d09f01f3c9fba25966ff1231f5d6adb4
Andreas Müller <schnitzeltony@gmail.com>
2023-03-28 06:18:53 +0200

    Get vkeyboard demo to start
    ZLineEdit fields are again pain within layout: They are not visible.
    We should start to replace them by something simpler as in [1] and merge one
    by one. But this is about virtual keyboard - so let's postpone.
    
    https://github.com/schnitzeltony/qtvirtualkeyboard-demo/blob/master/qml/controls/TextFieldEx.qml
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/zvkeyboard/zvkeyboardlayout.cpp
+++ libs/zvkeyboard/zvkeyboardlayout.cpp
@@ -3,5 +3,5 @@
 
 void ZVKeyboardLayout::setKeyboardLayoutEnvironment()
 {
-    qputenv("import QtQuick.VirtualKeyboard 2.1QT_VIRTUALKEYBOARD_LAYOUT_PATH", QByteArray(QML_SRC_PATH));
+    qputenv("QT_VIRTUALKEYBOARD_LAYOUT_PATH", QByteArray(QML_SRC_PATH));
 }
