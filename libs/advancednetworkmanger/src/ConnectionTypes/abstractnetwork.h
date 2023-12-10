79013c9c174e09b058a667e1f19221476f1d505b
Andreas Müller <schnitzeltony@gmail.com>
2023-12-10 20:31:49 +0100

    abstractnetwork.h: Do not make all methods virtual / rearrange code groups
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/advancednetworkmanger/src/ConnectionTypes/abstractnetwork.h
+++ libs/advancednetworkmanger/src/ConnectionTypes/abstractnetwork.h
@@ -50,16 +50,26 @@ class AbstractNetwork : public QObject
 public:
     AbstractNetwork();
     virtual bool init(ConnectionList &p_list, DeviceManager &p_devManager) = 0;
+public slots:
+    virtual void addConnection(const QString &connection);
+    virtual void removeConnection(const QString &connection);
+    virtual void addDevice(NetworkManager::Device::Type p_type, QString p_device);
+    virtual void removeDevice(QString p_device);
+
+    void ipv4Change();
+    void connectionActivated(const QString &p_path);
+    void connectionDeactivate(const QString &p_path);
+
 protected:
-    virtual void connectionActivated(const QString &p_path);
-    virtual void connectionDeactivate(const QString &p_path);
-    virtual void addConnectionToList(NetworkManager::Connection::Ptr p_con, connectionItem conItem);
-    virtual void findAvailableConnections(QString &p_uni);
-    virtual void findStoredConnections();
     virtual connectionItem CreateConItem(NetworkManager::Connection::Ptr p_con) = 0;
-    virtual bool isConnectionActive(QString p_path);
-    virtual QString getIpv4(QString p_path);
-protected:
+    void addAvailabelConnection(const QString &p_devPath, const QString &p_connection);
+    void removeAvailabelConnection(const QString &p_devPath, const QString &p_connection);
+    void addConnectionToList(NetworkManager::Connection::Ptr p_con, connectionItem conItem);
+    void findAvailableConnections(QString &p_uni);
+    void findStoredConnections();
+    bool isConnectionActive(QString p_path);
+    QString getIpv4(QString p_path);
+
     ConnectionList* m_list;
     QMap<QString,ConStruct> m_conList;
     QMap<QString,AconStruct> m_aConList;
@@ -68,16 +78,9 @@ protected:
     NetworkManager::Device::Type m_type;
     NetworkManager::Setting::SettingType m_setType;
 
-public slots:
-    virtual void addConnection(const QString &connection);
-    virtual void removeConnection(const QString &connection);
-    virtual void addAvailabelConnection(const QString &p_devPath, const QString &p_connection);
-    virtual void removeAvailabelConnection(const QString &p_devPath, const QString &p_connection);
-    virtual void addDevice(NetworkManager::Device::Type p_type, QString p_device);
-    virtual void removeDevice(QString p_device);
-    virtual void update(QString path);
+private:
+    void update(QString path);
     void stateChangeReason(QString path, NetworkManager::ActiveConnection::State state,NetworkManager::ActiveConnection::Reason reason);
-    void ipv4Change();
 };
 
 #endif // ABSTRACTNETWORK_H
