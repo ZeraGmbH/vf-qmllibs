dfab8f86e8e9c7af187d499afa564098869955d9
Andreas Müller <schnitzeltony@gmail.com>
2025-02-27 11:42:34 +0100

    Timedate1Connection: Support runtime property change notifications
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/timedate1/timedate1connection.h
+++ libs/datetime-setter/timedate1/timedate1connection.h
@@ -2,6 +2,8 @@
 #define TIMEDATE1CONNECTION_H
 
 #include "timedate1interface.h"
+#include <QStringList>
+#include <QVariantMap>
 
 class Timedate1Connection : public QObject
 {
@@ -12,17 +14,28 @@ public:
     const QStringList &getAvailTimezones() const;
     bool getNtpAvailable() const ;
     bool getNtpSynced() const;
+    bool getNtpActive() const;
 signals:
     void sigAvailTimezonesChanged();
-    void sigCanNtpChanged();
-    void sigIsNtpSyncedChanged();
+    void sigNtpAvailableChanged();
+    void sigNtpSyncedChanged();
+    void sigNtpActiveChanged();
 
+private slots:
+    void onPropertiesChanged(const QString &interface,
+                             const QVariantMap &changedProperties,
+                             const QStringList &invalidatedProperties);
 private:
-    void updateProperties();
+    void initProperties();
+    void updateNtpAvailable(bool available);
+    void updateNtpSynced(bool synced);
+    void updateNtpActive(bool active);
+
     std::unique_ptr<org::freedesktop::timedate1> m_timedateInterface;
     QStringList m_timezonesAvailable;
     bool m_ntpAvailable = false;
     bool m_ntpSynced = false;
+    bool m_ntpActive = false;
 };
 
 #endif // TIMEDATE1CONNECTION_H
