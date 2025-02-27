231e1bbee6256759abefe5ed0c193712d06a2102
Andreas Müller <schnitzeltony@gmail.com>
2025-02-27 11:42:34 +0100

    Timedate1Connection: Support runtime property change notifications
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/timedate1/timedate1connection.cpp
+++ libs/datetime-setter/timedate1/timedate1connection.cpp
@@ -1,12 +1,25 @@
 #include "timedate1connection.h"
 
+static const char* dbusTimdate1Name = org::freedesktop::timedate1::staticInterfaceName();
+static const char* dbusTimedate1Path = "/org/freedesktop/timedate1";
+static QDBusConnection dbusSystemBus = QDBusConnection::systemBus();
+
 void Timedate1Connection::start()
 {
     if (!m_timedateInterface) {
         m_timedateInterface = std::make_unique<org::freedesktop::timedate1>(
-            org::freedesktop::timedate1::staticInterfaceName(),
-            "/org/freedesktop/timedate1",
-            QDBusConnection::systemBus());
+            dbusTimdate1Name,
+            dbusTimedate1Path,
+            dbusSystemBus);
+
+        dbusSystemBus.connect(
+            dbusTimdate1Name,
+            dbusTimedate1Path,
+            "org.freedesktop.DBus.Properties",
+            "PropertiesChanged",
+            this,
+            SLOT(onPropertiesChanged(QString, QVariantMap, QStringList)) // Qt5 connection not supported
+            );
 
         QDBusPendingReply<QStringList> reply = m_timedateInterface->ListTimezones();
         auto watcher = new QDBusPendingCallWatcher(reply, this);
@@ -14,7 +27,7 @@ void Timedate1Connection::start()
             watcher->deleteLater();
             m_timezonesAvailable = reply.value();
             emit sigAvailTimezonesChanged();
-            updateProperties();
+            initProperties();
         });
     }
 }
@@ -34,17 +47,53 @@ bool Timedate1Connection::getNtpSynced() const
     return m_ntpSynced;
 }
 
-void Timedate1Connection::updateProperties()
+void Timedate1Connection::onPropertiesChanged(const QString &interface,
+                                              const QVariantMap &changedProperties,
+                                              const QStringList &invalidatedProperties)
 {
-    const bool newCanNtp = m_timedateInterface->canNTP();
-    if (m_ntpAvailable != newCanNtp) {
-        m_ntpAvailable = newCanNtp;
-        emit sigCanNtpChanged();
-    }
+    Q_UNUSED(invalidatedProperties)
+    if (interface == dbusTimdate1Name) {
+        auto iterNtpAvailable = changedProperties.constFind("CanNTP");
+        if(iterNtpAvailable != changedProperties.constEnd())
+            updateNtpAvailable(iterNtpAvailable.value().toBool());
 
-    const bool newIsNtpSynced = m_timedateInterface->nTPSynchronized();
-    if (m_ntpSynced != newIsNtpSynced) {
-        m_ntpSynced = newIsNtpSynced;
-        emit sigIsNtpSyncedChanged();
+        auto iterNtpSynced = changedProperties.constFind("NTPSynchronized");
+        if(iterNtpSynced != changedProperties.constEnd())
+            updateNtpSynced(iterNtpSynced.value().toBool());
+
+        auto iterNtpActive = changedProperties.constFind("NTP");
+        if(iterNtpActive != changedProperties.constEnd())
+            updateNtpActive(iterNtpActive.value().toBool());
+    }
+}
+
+void Timedate1Connection::initProperties()
+{
+    updateNtpAvailable(m_timedateInterface->canNTP());
+    updateNtpSynced(m_timedateInterface->nTPSynchronized());
+    updateNtpActive(m_timedateInterface->nTP());
+}
+
+void Timedate1Connection::updateNtpAvailable(bool available)
+{
+    if (m_ntpAvailable != available) {
+        m_ntpAvailable = available;
+        emit sigNtpAvailableChanged();
+    }
+}
+
+void Timedate1Connection::updateNtpSynced(bool synced)
+{
+    if (m_ntpSynced != synced) {
+        m_ntpSynced = synced;
+        emit sigNtpSyncedChanged();
+    }
+}
+
+void Timedate1Connection::updateNtpActive(bool active)
+{
+    if(m_ntpActive != active) {
+        m_ntpActive = active;
+        emit sigNtpActiveChanged();
     }
 }
