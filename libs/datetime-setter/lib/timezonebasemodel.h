aa0e44b6a564f578a4564282a7c42d53a02af87f
Andreas Müller <schnitzeltony@gmail.com>
2025-03-05 21:26:28 +0100

    TimezoneBaseModel: Remove default parameter in ctor
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/lib/timezonebasemodel.h
+++ libs/datetime-setter/lib/timezonebasemodel.h
@@ -2,7 +2,6 @@
 #define TIMEZONEBASEMODEL_H
 
 #include "abstracttimedate1connection.h"
-#include "timedate1connection.h"
 #include "timezonetranslations.h"
 #include <QAbstractListModel>
 #include <QList>
@@ -12,8 +11,7 @@ class TimezoneBaseModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
-    explicit TimezoneBaseModel(std::shared_ptr<AbstractTimedate1Connection> timedateConnection =
-                               std::make_shared<Timedate1Connection>());
+    explicit TimezoneBaseModel(std::shared_ptr<AbstractTimedate1Connection> timedateConnection);
     QHash<int, QByteArray> roleNames() const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
@@ -30,7 +28,6 @@ public:
 private slots:
     void fillModel();
 private:
-
     QStringList m_timezones;
     std::shared_ptr<AbstractTimedate1Connection> m_timedateConnection;
     TimezoneTranslations m_translation;
