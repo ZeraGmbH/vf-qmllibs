bc3c3ab9c80c22b1df17c1f228dcfdd962e5cf4f
Andreas Müller <schnitzeltony@gmail.com>
2025-03-07 09:10:06 +0100

    Introduce TimezoneModelsFacade as an initial draft for an interface to QML
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ libs/datetime-setter/lib/timezonemodelsfacade.h
@@ -0,0 +1,50 @@
+#ifndef TIMEZONEMODELSFACADE_H
+#define TIMEZONEMODELSFACADE_H
+
+#include "abstracttimedate1connection.h"
+#include "timezonemodelbase.h"
+#include "timezonemodelcityfiltered.h"
+#include "timezonemodelregion.h"
+#include "timezonetranslations.h"
+#include <QAbstractListModel>
+#include <memory>
+
+class TimezoneModelsFacade : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TimezoneModelsFacade(std::shared_ptr<AbstractTimedate1Connection> timedateConnection,
+                                  std::shared_ptr<TimezoneTranslations> translations);
+
+    Q_PROPERTY(QAbstractListModel* regionModel READ getRegionModel CONSTANT)
+    QAbstractListModel* getRegionModel() const;
+
+    Q_PROPERTY(QString regionSelected READ getRegionSelected WRITE setRegionSelected NOTIFY sigRegionSelectedChanged FINAL)
+    QString getRegionSelected() const;
+    void setRegionSelected(const QString &region);
+
+    Q_PROPERTY(QAbstractListModel* cityModel READ getCityModel CONSTANT)
+    QAbstractListModel* getCityModel() const;
+
+    Q_PROPERTY(QString citySelected READ getCitySelected WRITE setCitySelected NOTIFY sigCitySelectedChanged FINAL)
+    QString getCitySelected() const;
+    void setCitySelected(const QString &city);
+
+    Q_PROPERTY(bool canApply READ canApply NOTIFY canApplyChanged FINAL)
+    bool canApply() const;
+    Q_INVOKABLE void doApply();
+
+signals:
+    void sigRegionSelectedChanged();
+    void sigCitySelectedChanged();
+    void canApplyChanged();
+
+private:
+    std::shared_ptr<AbstractTimedate1Connection> m_timedateConnection;
+    std::shared_ptr<TimezoneTranslations> m_translations;
+    std::shared_ptr<TimezoneModelBase> m_modelBase;
+    std::shared_ptr<TimezoneModelRegion> m_modelRegion;
+    std::shared_ptr<TimezoneModelCityFiltered> m_modelCities;
+};
+
+#endif // TIMEZONEMODELSFACADE_H
