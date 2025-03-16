065bb00892c909609649cc56c455db49d69fae50
Andreas Müller <schnitzeltony@gmail.com>
2025-03-16 23:12:45 +0100

    TimezoneComboRow: Add undo / apply buttons
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/zeracomponents/src/zeracomponents.cpp
+++ libs/zeracomponents/src/zeracomponents.cpp
@@ -1,5 +1,6 @@
 #include "zeracomponents.h"
 #include "timedateqmlwrapper.h"
+#include <zerafa.h>
 
 bool ZeraComponents::m_wasRegistered = false;
 
@@ -7,6 +8,9 @@ void ZeraComponents::registerQml(QQmlApplicationEngine &engine)
 {
     if(!m_wasRegistered) {
         TimedateQmlWrapper::registerQml();
+        // Unfortunataley FontAwesomeQml does not work without further investigation...
+        ZeraFa::registerQml(engine);
+
         QString debugQmlPath = QStringLiteral(QML_SRC_PATH);
         engine.addImportPath(debugQmlPath);
         m_wasRegistered = true;
