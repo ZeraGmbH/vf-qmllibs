42db8e47239953d990da339060e9acd8b38889b9
Andreas Müller <schnitzeltony@gmail.com>
2025-03-21 15:21:43 +0100

    abstracttimedate1connection.h: Use Q_SIGNAL
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/lib/abstracttimedate1connection.h
+++ libs/datetime-setter/lib/abstracttimedate1connection.h
@@ -11,28 +11,27 @@ class AbstractTimedate1Connection : public QObject
     Q_OBJECT
 public:
     virtual void start() = 0;
+    Q_SIGNAL void sigStarted();
 
     virtual const QStringList &getAvailTimezones() const = 0;
+    Q_SIGNAL void sigAvailTimezonesChanged();
+
     virtual QString getTimeszone() const = 0;
     virtual void setTimezone(const QString &timezone) = 0;
+    Q_SIGNAL void sigTimezoneChanged();
 
     virtual bool getNtpAvailable() const = 0;
+    Q_SIGNAL void sigNtpAvailableChanged();
+
     virtual bool getNtpSynced() const = 0;
+    Q_SIGNAL void sigNtpSyncedChanged();
+
     virtual bool getNtpActive() const = 0;
     virtual void setNtpActive(bool active) = 0;
+    Q_SIGNAL void sigNtpActiveChanged();
 
     virtual void setDateTime(const QDateTime dateTime) = 0;
-signals:
-    void sigStarted();
-
-    void sigAvailTimezonesChanged();
-    void sigTimezoneChanged();
-
-    void sigNtpAvailableChanged();
-    void sigNtpSyncedChanged();
-    void sigNtpActiveChanged();
-
-    void sigDateTimeChanged(bool ok);
+    Q_SIGNAL void sigDateTimeChanged(bool ok);
 };
 
 typedef std::shared_ptr<AbstractTimedate1Connection> AbstractTimedate1ConnectionPtr;
