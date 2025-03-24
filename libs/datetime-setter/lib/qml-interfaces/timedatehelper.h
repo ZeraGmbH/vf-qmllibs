29ab306878695541c5bbc2c58e065abf66e2dac9
Andreas Müller <schnitzeltony@gmail.com>
2025-03-24 18:14:43 +0100

    Add TimeDateHelper::setAllDateTime
    Offer QML a way to apply all datetime settings changed
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/lib/qml-interfaces/timedatehelper.h
+++ libs/datetime-setter/lib/qml-interfaces/timedatehelper.h
@@ -2,6 +2,7 @@
 #define TIMEDATEHELPER_H
 
 #include "abstracttimedate1connection.h"
+#include "tasktemplate.h"
 #include <QObject>
 
 class TimeDateHelper : public QObject
@@ -18,8 +19,15 @@ public:
     Q_SIGNAL void sigNtpActiveChanged();
     bool getNtpActive() const;
 
+    Q_INVOKABLE void setAllDateTime(const QString &timezone,
+                                    bool ntpActive,
+                                    int year, int month, int day,
+                                    int hour, int minute, int second);
+    Q_SIGNAL void sigAllDateTimeSet(QStringList errorMessages);
 private:
     AbstractTimedate1ConnectionPtr m_timedateConnection;
+    TaskTemplatePtr m_taskSetAll;
+    std::shared_ptr<QStringList> m_errorMessages;
 };
 
 #endif // TIMEDATEHELPER_H
