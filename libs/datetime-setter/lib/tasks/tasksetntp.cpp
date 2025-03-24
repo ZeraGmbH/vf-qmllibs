1ae673874ce1760320ff32c951997bb64ace1960
Andreas Müller <schnitzeltony@gmail.com>
2025-03-24 11:17:41 +0100

    Add TaskSetNtp::create
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/datetime-setter/lib/tasks/tasksetntp.cpp
+++ libs/datetime-setter/lib/tasks/tasksetntp.cpp
@@ -1,4 +1,14 @@
 #include "tasksetntp.h"
+#include <taskdecoratortimeout.h>
+
+TaskTemplatePtr TaskSetNtp::create(AbstractTimedate1ConnectionPtr timedateConnection,
+                                   bool ntpActive,
+                                   std::function<void ()> additionalErrorHandler, int timeoutMs)
+{
+    return TaskDecoratorTimeout::wrapTimeout(timeoutMs,
+                                             std::make_unique<TaskSetNtp>(timedateConnection, ntpActive),
+                                             additionalErrorHandler);
+}
 
 TaskSetNtp::TaskSetNtp(AbstractTimedate1ConnectionPtr timedateConnection,
                        bool ntpActive) :
