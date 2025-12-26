c3dd89b0c9c329c7ae12596e6f56f964f4e42e34
Andreas Müller <schnitzeltony@gmail.com>
2025-12-26 23:01:34 +0100

    zeracomponents: Add hack to get VUs working on Qt5 and Qt6
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/zeracomponents/src/qml/ZeraComponents/SacVuBase.qml
+++ libs/zeracomponents/src/qml/ZeraComponents/SacVuBase.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.14
 import QtQuick.Controls 2.14
 import QtGraphicalEffects 1.14
+import Qt5Compat.GraphicalEffects 1.0
 
 SacVuDefaults {
     id: root
--- libs/zeracomponents/src/zeracomponents.cpp
+++ libs/zeracomponents/src/zeracomponents.cpp
@@ -7,6 +7,14 @@ bool ZeraComponents::m_wasRegistered = false;
 void ZeraComponents::registerQml(QQmlApplicationEngine &engine)
 {
     if(!m_wasRegistered) {
+        // Add a dummy component so QML can import both Qt6 / Qt5 GraphicalEffects variants.
+        // This has to go once we get rid of Qt5. Stolen from
+        // https://stackoverflow.com/questions/69602129/use-qt5compat-graphicaleffects-in-qt6-and-qtgraphicaleffects-in-qt5-in-the-same
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+        qmlRegisterModule("QtGraphicalEffects", 1, 14);
+#else
+        qmlRegisterModule("Qt5Compat.GraphicalEffects", 1, 0);
+#endif
         TimedateQmlWrapper::registerQml();
         // Unfortunataley FontAwesomeQml does not work without further investigation...
         ZeraFa::registerQml(engine);
