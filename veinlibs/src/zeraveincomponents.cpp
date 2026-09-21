d920c3bb3b6205de3f7d9fa1ecaabab4b3ba0c19
Andreas Müller <schnitzeltony@gmail.com>
2026-09-21 20:54:43 +0200

    veinlibs: Register required ZeraComponents & QmlFileIO
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- veinlibs/src/zeraveincomponents.cpp
+++ veinlibs/src/zeraveincomponents.cpp
@@ -1,10 +1,15 @@
 #include "zeraveincomponents.h"
+#include "zeracomponents.h"
+#include <qmlfileio.h>
 
 bool ZeraVeinComponents::m_wasRegistered = false;
 
 void ZeraVeinComponents::registerQml(QQmlApplicationEngine &engine)
 {
     if(!m_wasRegistered) {
+        QmlFileIO::registerQMLSingleton();
+        ZeraComponents::registerQml(engine);
+
         QString debugQmlPath = QStringLiteral(QML_SRC_PATH);
         engine.addImportPath(debugQmlPath);
         m_wasRegistered = true;
